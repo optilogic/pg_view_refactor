04efefcb061d770a5ffdd67163906a76aef380a4
Joseph Fullmer <joe.fullmer@optilogic.com>
2024-12-12 23:46:08 -0700

    shuffle column order to my liking. :)

--- vw_rdcinvtimeseriesfull_unitsandvalue.sql
+++ vw_rdcinvtimeseriesfull_unitsandvalue.sql
@@ -2,17 +2,17 @@ EXPLAIN WITH eodInventoryOnHand AS (
     SELECT  DISTINCT ON (inv.facilityname, inv.productname, inv."time"::date) 
         inv.facilityname,
         inv.productname,
-        inv.inventoryonhandquantity,
-        inv."time"::date AS simdate
+        inv."time"::date AS simdate,
+        inv.inventoryonhandquantity
     FROM simulationinventoryonhandreport inv
     WHERE inv.scenarioname = 'RDC HW'
     ORDER BY inv.facilityname, inv.productname, inv."time"::date, inv."time" DESC
 ), eodInventoryValues AS (
     SELECT eod.facilityname,
         eod.productname,
+        ip.flowpath,
         eod.simdate,
         eod.inventoryonhandquantity AS eodinventory,
-        ip.flowpath,
         p.unitvalue::numeric * eod.inventoryonhandquantity AS valueonhand
     FROM eodInventoryOnHand eod
     LEFT JOIN inventorypolicies ip on eod.facilityname = replace(replace(lower(ip.facilityname), 'w12901x', 'w12901'), 'w12901', 'w12901x')
@@ -21,8 +21,8 @@ EXPLAIN WITH eodInventoryOnHand AS (
 )
 SELECT 'RDC HW' AS scenarioname,
     flowpath,
+    simdate,
     sum(eodinventory) AS unitsonhand,
-    sum(valueonhand) AS valueonhand,
-    simdate
+    sum(valueonhand) AS valueonhand
 FROM eodInventoryValues
 GROUP BY flowpath, simdate
