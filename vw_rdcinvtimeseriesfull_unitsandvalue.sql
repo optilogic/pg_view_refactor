ddd32160e9ed1e96d41fa017d67738141d1ddc7c
Joseph Fullmer <joe.fullmer@optilogic.com>
2024-12-12 23:39:28 -0700

    use meaningful names for cte subqueries

--- vw_rdcinvtimeseriesfull_unitsandvalue.sql
+++ vw_rdcinvtimeseriesfull_unitsandvalue.sql
@@ -1,4 +1,4 @@
-EXPLAIN WITH df2 AS (
+EXPLAIN WITH eodInventoryOnHand AS (
     SELECT  DISTINCT ON (inv.scenarioname, inv.facilityname, inv.productname, inv."time"::date) 
         inv.scenarioname,
         inv.facilityname,
@@ -8,23 +8,23 @@ EXPLAIN WITH df2 AS (
     FROM simulationinventoryonhandreport inv
     WHERE inv.scenarioname = 'RDC HW'
     ORDER BY inv.scenarioname, inv.facilityname, inv.productname, inv."time"::date, inv."time" DESC
-), df5 AS (
-    SELECT df2.facilityname,
-        df2.productname,
-        df2.scenarioname,
-        df2.simdate,
-        df2.inventoryonhandquantity AS eodinventory,
+), eodInventoryValues AS (
+    SELECT eod.facilityname,
+        eod.productname,
+        eod.scenarioname,
+        eod.simdate,
+        eod.inventoryonhandquantity AS eodinventory,
         ip.flowpath,
-        p.unitvalue::numeric * df2.inventoryonhandquantity AS valueonhand
-    FROM df2
-    LEFT JOIN inventorypolicies ip on df2.facilityname = replace(replace(lower(ip.facilityname), 'w12901x', 'w12901'), 'w12901', 'w12901x')
-        and df2.productname = lower(ip.productname)
-    LEFT JOIN products p ON df2.productname = lower(p.productname)
+        p.unitvalue::numeric * eod.inventoryonhandquantity AS valueonhand
+    FROM eodInventoryOnHand eod
+    LEFT JOIN inventorypolicies ip on eod.facilityname = replace(replace(lower(ip.facilityname), 'w12901x', 'w12901'), 'w12901', 'w12901x')
+        and eod.productname = lower(ip.productname)
+    LEFT JOIN products p ON eod.productname = lower(p.productname)
 )
 SELECT scenarioname,
     flowpath,
     sum(eodinventory) AS unitsonhand,
     sum(valueonhand) AS valueonhand,
     simdate
-FROM df5
+FROM eodInventoryValues
 GROUP BY scenarioname, flowpath, simdate
