bad5cfaa1333f9c9c9c3a04f5e5013fd13320cee
Joseph Fullmer <joe.fullmer@optilogic.com>
2024-12-12 23:23:35 -0700

    df5 still has no predicate, so combine with preceding subquery

--- vw_rdcinvtimeseriesfull_unitsandvalue.sql
+++ vw_rdcinvtimeseriesfull_unitsandvalue.sql
@@ -8,27 +8,19 @@ EXPLAIN WITH df2 AS (
     FROM simulationinventoryonhandreport inv
     WHERE inv.scenarioname = 'RDC HW'
     ORDER BY inv.scenarioname, inv.facilityname, inv.productname, inv."time"::date, inv."time" DESC
-), df5_1 AS (
+), df5 AS (
     SELECT df2.facilityname,
         df2.productname,
         df2.scenarioname,
         df2.simdate,
         df2.inventoryonhandquantity AS eodinventory,
-        ip.flowpath
+        ip.flowpath,
+        p.unitvalue::numeric as unitvalue,
+        p.unitvalue::numeric * df2.inventoryonhandquantity AS valueonhand
     FROM df2
     LEFT JOIN inventorypolicies ip on df2.facilityname = replace(replace(lower(ip.facilityname), 'w12901x', 'w12901'), 'w12901', 'w12901x')
         and df2.productname = lower(ip.productname)
-), df5 AS (
-    SELECT df5_1.productname,
-        df5_1.scenarioname,
-        df5_1.facilityname,
-        df5_1.eodinventory,
-        df5_1.simdate,
-        df5_1.flowpath,
-        p.unitvalue::numeric as unitvalue,
-        p.unitvalue::numeric * df5_1.eodinventory AS valueonhand
-    FROM df5_1
-    LEFT JOIN products p ON df5_1.productname = lower(p.productname)
+    LEFT JOIN products p ON df2.productname = lower(p.productname)
 ), df6 AS (
     SELECT df5.scenarioname,
         df5.flowpath,
